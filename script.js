f3840fba3d7bba2fe0dcc9a7ea3d3990c2745568
s4rmier <135184315+s4rmier@users.noreply.github.com>
2023-06-20 09:45:58 -0700

    Assignment 2

---
+++ script.js
@@ -0,0 +1,56 @@
+// Gain access to the DOM elements
+let prodName = document.getElementById("prod-name");
+let prodPrice = document.getElementById("prod-price");
+let prodQty = document.getElementById("prod-qty");
+let subTotal = document.getElementById("sub-total");
+let grandTotal = document.getElementById("grand-total");
+let salesTax = document.getElementById("sales-tax");
+let button = document.getElementById("button");
+
+// Enable button to perform a task of calling the functions
+button.addEventListener("click", () => {
+  prodName.textContent = prompt("Enter product name: ");
+  validateUserInput(prodPrice);
+  validateUserInput(prodQty);
+  updateDOM();
+});
+
+// Same as button
+document.addEventListener("keydown", function (event) {
+  if (event.key === "F3" || event.keyCode === 114) {
+    prodName.textContent = prompt("Enter product name: ");
+    validateUserInput(prodPrice);
+    validateUserInput(prodQty);
+    updateDOM();
+  }
+});
+
+// validating whether the user's input is a number or not
+let validateUserInput = (input) => {
+  let userInput;
+  let isValid = false;
+
+  while (!isValid) {
+    userInput =
+      input === prodPrice ? prompt("Enter price:") : prompt("Enter quantity :");
+    if (!isNaN(userInput)) {
+      isValid = true;
+      input.textContent = userInput;
+    } else {
+      alert("Please enter a valid number");
+    }
+  }
+};
+
+// update the DOM elements if the previous function executions are valid
+let updateDOM = () => {
+  let price = parseFloat(prodPrice.textContent);
+  let qty = parseFloat(prodQty.textContent);
+  subTotal.textContent = price * qty;
+
+  let taxes = parseFloat(subTotal.textContent) * 0.075;
+  salesTax.textContent = taxes;
+
+  let total = parseFloat(subTotal.textContent) * 1.075;
+  grandTotal.textContent = total;
+};
